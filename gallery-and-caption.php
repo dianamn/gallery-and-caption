e495185eeb86796efad425259184d2010e96aa63
tiko35 <gtgtigran35@gmail.com>
2017-02-07 21:10:12 +0400

    Added mosaic view.

--- gallery-and-caption.php
+++ gallery-and-caption.php
@@ -4,9 +4,9 @@
 Plugin Name: Huge IT Photo Gallery
 Plugin URI: http://huge-it.com/wordpress-photo-gallery/
 Description: Photo Gallery is advanced solution for WordPress Gallery users. 7 advanced and customizable views will help to create beautiful content in minutes.
-Version: 2.0.3
+Version: 2.0.4
 Author: Huge-IT
-Author: http://huge-it.com/
+Author URI: http://huge-it.com/
 License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 */
 
@@ -22,7 +22,7 @@ if ( ! class_exists( 'Photo_Gallery_WP' ) ) :
          * Version of plugin
          * @var String
          */
-        public $version = "2.0.3";
+        public $version = "2.0.4";
 
         /**
          * Instance of Gallery_Img_Admin class to manage admin
